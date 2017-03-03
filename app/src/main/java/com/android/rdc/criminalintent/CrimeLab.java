b75bdf4a463db9ac2f747d3b69e73d96458200e9
ivanljt <596476418@qq.com>
2017-03-03 20:45:51 +0800

    使用了SQlite进行 本地持久化存储

--- app/src/main/java/com/android/rdc/criminalintent/CrimeLab.java
+++ app/src/main/java/com/android/rdc/criminalintent/CrimeLab.java
@@ -1,38 +1,48 @@
 package com.android.rdc.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.android.rdc.criminalintent.database.CrimeBaseHelper;
+import com.android.rdc.criminalintent.database.CrimeCursorWrapper;
+import com.android.rdc.criminalintent.database.CrimeDbSchema.CrimeTable;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 /**
  * Created by Administrator on 2017/2/27 0027.
  */
-//，该对象是一个数据集中存储池，用来存储 Crime 对象。
 public class CrimeLab implements Serializable {
-    private static CrimeLab sCrimeLab;
+    private volatile static CrimeLab sCrimeLab; //声明为volatile 保证new过程中的 原子性
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mSQLiteDatabase;
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
-//        for (int i = 0; i < 100; i++) {
-//            Crime crime = new Crime();
-//            crime.setTitle("Crime #" + i);
-//            crime.setSolved(i % 2 == 0);
-//            mCrimes.add(crime);
-//        }
+        mContext = context.getApplicationContext();
+        mSQLiteDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
     }
-    public void addCrime(Crime c){
-        mCrimes.add(c);
+
+    public void addCrime(Crime c) {
+        ContentValues contentValues = getContentValue(c);
+        mSQLiteDatabase.insert(CrimeTable.NAME, null, contentValues);
+    }
+
+    public void updateCrime(Crime crime) {
+        ContentValues contentValues = getContentValue(crime);
+        String uuidString = crime.getId().toString();
+        mSQLiteDatabase.update(CrimeTable.NAME, contentValues, CrimeTable.Cols.UUID + " = ?", new String[]{uuidString});
     }
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
-            synchronized (CrimeLab.class){
-                if (sCrimeLab == null){
+            synchronized (CrimeLab.class) {
+                if (sCrimeLab == null) {
                     sCrimeLab = new CrimeLab(context);
                 }
             }
@@ -40,23 +50,60 @@ public class CrimeLab implements Serializable {
         return sCrimeLab;
     }
 
-    public void deleteCrime(UUID crimeID){
-        if (crimeID == null){
+    public void deleteCrime(UUID crimeID) {
+        if (crimeID == null) {
             return;
         }
-        mCrimes.remove(getCrime(crimeID));
+//        mCrimes.remove(getCrime(crimeID));
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
+        CrimeCursorWrapper cursor = queryCrime(CrimeTable.Cols.UUID + " = ?", new String[]{id.toString()});
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
             }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
         }
-        return null;
     }
 
     public List<Crime> getCrimes() {
+        mCrimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = queryCrime(null, null);
+        try {
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                mCrimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
         return mCrimes;
+    }
+
+    private static ContentValues getContentValue(Crime crime) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(CrimeTable.Cols.UUID, crime.getId().toString());
+        contentValues.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
+        contentValues.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        contentValues.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        return contentValues;
+    }
+
+    public CrimeCursorWrapper queryCrime(String whereClause, String[] keys) {
+        Cursor cursor = mSQLiteDatabase.query(
+                CrimeTable.NAME,
+                null, // Columns - null means selecting all columns
+                whereClause,
+                keys,
+                null,
+                null,
+                null
+        );
+        return new CrimeCursorWrapper(cursor);
     }
 }
